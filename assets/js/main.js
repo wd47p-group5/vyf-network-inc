852792cfd91670106233edbecee6452a41a8ce93
xtian <benedictramirez78@gmail.com>
2023-04-04 00:08:18 +0800

    add back-to-top function

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 500) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#0F9D58 ${scrollValue}%,  #F4B400 ${scrollValue}%)`;
+};    
+  
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
