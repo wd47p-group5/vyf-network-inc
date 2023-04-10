b1854997726c273218295ef91989ee0d56854852
Ed Benedict Quia <edbenedictquia@gmail.com>
2023-04-10 19:05:15 +0800

    Finish video page

---
+++ assets/js/product-brands/cisco.js
@@ -0,0 +1,13 @@
+export let brandProducts = [
+    {
+      type: "voip",
+      name: "Desk Camera 4K",
+      src: "https://www.cisco.com/c/en/us/products/collaboration-endpoints/webex-desk-camera/index/jcr:content/Grid/category_atl/layout-category-atl/spotlight_457489289.img.png/1665391260292.png",
+    },
+    {
+      type: "voip",
+      name: "Desk Camera 1080p",
+      src: "https://www.cisco.com/c/en/us/products/collaboration-endpoints/webex-desk-camera/index/jcr:content/Grid/category_atl/layout-category-atl/spotlight_1222958901.img.png/1665391282066.png",
+    },
+  ];
+  
