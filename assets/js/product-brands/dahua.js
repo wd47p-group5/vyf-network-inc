f1187640cbfae8f4e9b0ce2721d1540be6503cea
Ed Benedict Quia <edbenedictquia@gmail.com>
2023-04-13 21:39:12 +0800

    Add description Add description, brands html files and fixed cctv.html

---
+++ assets/js/product-brands/dahua.js
@@ -0,0 +1,26 @@
+export let brandProducts = [
+  {
+    type: "cctv",
+    name: "IPC-HDW4831EM-ASE",
+    src: "https://www.dahuasecurity.com/asset/upload/upfiles/IPC-HDW4831EM-ASE_thumb.png",
+    description: "8MP IR Eyeball Network Camera with 1/2.5” 8Megapixel progressive scan STARVISTM CMOS and Smart detection support",
+  },
+  {
+    type: "cctv",
+    name: "IPC-HDBW4831E-ASE",
+    src: "https://www.dahuasecurity.com/asset/upload/upfiles/IPC-HDBW4831E-ASE_thumb.png",
+    description: "8MP IR Mini Dome Network Camera with 1/2.5” 8Megapixel progressive scan STARVISTM CMOS and H.265&H.264 triple-stream encoding",
+  },
+  {
+    type: "cctv",
+    name: "IPC-HDW5831R-ZE",
+    src: "https://www.dahuasecurity.com/asset/upload/upfiles/IPC-HDW5831R-ZE_thumb.png",
+    description: "8MP WDR IR Eyeball Network Camera with Multiple network monitoring: Web viewer, CMS(DSS/PSS) & DMSS and 2.7mm ~12mm motorized lens",
+  },
+  {
+    type: "cctv",
+    name: "IPC-HDBW5831R-ZE",
+    src: "https://www.dahuasecurity.com/asset/upload/upfiles/IPC-HDBW5831R-ZE_thumb.png",
+    description: "8MP WDR IR Dome Network Camera with 1/2.5” 8Megapixel progressive scan STARVIS™ CMOS and Day/Night(ICR), 3DNR, AWB, AGC, BLC",
+  },
+];
