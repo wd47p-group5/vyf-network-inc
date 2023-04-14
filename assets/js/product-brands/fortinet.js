50a685028c681d309740f5458ac3dd493b9ba1fc
Khim1111 <124416784+Khim1111@users.noreply.github.com>
2023-04-14 22:27:50 +0800

    Added fortinet products in security page

---
+++ assets/js/product-brands/fortinet.js
@@ -0,0 +1,27 @@
+export let brandProducts = [
+    {
+      type: "security",
+      name: "FortiMail",
+      src: "https://marvel-b1-cdn.bc0a.com/f00000000216283/www.fortinet.com/content/dam/fortinet/images/icons/products/demos/demo-laptop-fortimail.png",
+      description: "FortiMail helps your organization prevent, detect, and respond to email-based threats including spam, phishing, malware including ransomware, zero-day threats, impersonation, and Business Email Compromise (BEC) attacks",
+    },
+    {
+      type: "security",
+      name: "FortiNAC",
+      src: "https://marvel-b1-cdn.bc0a.com/f00000000216283/www.fortinet.com/content/dam/fortinet/images/icons/products/demos/demo-laptop-fortinac.png",
+      description: "FortiNAC provides protection against IoT threats, extends control to third-party devices, and orchestrates automatic response to a wide range of networking events.​",
+    },
+    {
+        type: "security",
+        name: "FortiDDoS",
+        src: "https://marvel-b1-cdn.bc0a.com/f00000000216283/www.fortinet.com/content/dam/fortinet/images/icons/products/demos/demo-laptop-fortiddos.png",
+        description: "FortiDDoS, Fortinet’s inline, purpose-built, DDoS protection solution, defends against business continuity disruptions caused by DDoS attacks.",
+      },
+      {
+        type: "security",
+        name: "FortiAuthenticator",
+        src: "https://marvel-b1-cdn.bc0a.com/f00000000216283/www.fortinet.com/content/dam/fortinet/images/icons/products/demos/demo-laptop-fortiauthenticator.png",
+        description: "Fortinet User Authentication provides you with the tools and capabilities for effective authentication, access, and identity management of users, devices, and guests or partners.",
+      },
+  ];
+  
