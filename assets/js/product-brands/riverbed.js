19538b42ee0e3cb1d450a677323fbf9ed39e76d2
Khim1111 <124416784+Khim1111@users.noreply.github.com>
2023-04-14 23:28:44 +0800

    Added Riverbed products in Security page

---
+++ assets/js/product-brands/riverbed.js
@@ -0,0 +1,27 @@
+export let brandProducts = [
+    {
+      type: "security",
+      name: "Alluvio Packet Analyzer Plus",
+      src: "https://store-images.s-microsoft.com/image/apps.12843.a467e107-7af8-4241-9a43-8cd32d59a507.eec3cc1e-8ea5-4fe2-a7ac-52bcc70109b2.a36b35c3-1d33-4e8d-9aa0-049dc4097bc3",
+      description: "Alluvio Packet Analyzer Plus speeds real-time network packet analysis and reporting of large trace files using an intuitive graphical user interface and a broad selection of pre-defined analysis views.",
+    },
+    {
+      type: "security",
+      name: "NetAuditor",
+      src: "https://store-images.s-microsoft.com/image/apps.12843.a467e107-7af8-4241-9a43-8cd32d59a507.eec3cc1e-8ea5-4fe2-a7ac-52bcc70109b2.a36b35c3-1d33-4e8d-9aa0-049dc4097bc3",
+      description: "Riverbed NetAuditor is a comprehensive network configuration management solution that dynamically creates and maintains infrastructure diagrams and reports configuration changes.  ",
+    },
+    {
+        type: "security",
+        name: "AWS Network Monitoring",
+        src: "https://store-images.s-microsoft.com/image/apps.12843.a467e107-7af8-4241-9a43-8cd32d59a507.eec3cc1e-8ea5-4fe2-a7ac-52bcc70109b2.a36b35c3-1d33-4e8d-9aa0-049dc4097bc3",
+        description: "High-level end-to-end flow visibility using AWS VPC Flow Logs. Deep-dive network and application packet-based visibility using AWS Traffic Mirroring for secure packet collection.",
+      },
+      {
+        type: "security",
+        name: "SteelHead Interceptor",
+        src: "https://store-images.s-microsoft.com/image/apps.12843.a467e107-7af8-4241-9a43-8cd32d59a507.eec3cc1e-8ea5-4fe2-a7ac-52bcc70109b2.a36b35c3-1d33-4e8d-9aa0-049dc4097bc3",
+        description: "SteelHead Interceptor enables customers to scale optimization solutions to support hundreds of thousands of end users across an enterprise, as well as very high bandwidth connections into the data center..",
+      },
+  ];
+  
