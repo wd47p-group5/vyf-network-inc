512e8ca03a4dc49b7113bd1ce273f010bb3292b3
Khim1111 <124416784+Khim1111@users.noreply.github.com>
2023-04-17 20:10:15 +0800

    Added vertiv products page

---
+++ assets/js/product-brands/vertiv.js
@@ -0,0 +1,27 @@
+export let brandProducts = [
+    {
+      type: "structuredCabling",
+      name: "LT410 Point Leak Detection Sensors",
+      src: "../../../../assets/images/products/cabling/vertiv/LT410.webp",
+      description: "These point detection modules install quickly and work simply for reliable warning of potentially hazardous moisture leaks at critical points under a raised floor. A simple two-wire connection signals the alarms to a Liebert® environmental unit monitor panel.",
+    },
+    {
+      type: "structuredCabling",
+      name: "Avocent HMX 5000",
+      src: "../../../../assets/images/products/cabling/vertiv/Avocent_HMX_5000.webp",
+      description: "The Avocent HMX Digital High Performance KVM system is designed for organizations that need to provide workers the ability to seamlessly access and share one or more computing resource(s) which are physically separated from their work environment",
+    },
+    {
+        type: "structuredCabling",
+        name: "GXT5 Lithium-Ion UPS 1000-3000 VA",
+        src: "../../../../assets/images/products/cabling/vertiv/GXT5 Lithium-Ion.webp",
+        description: "The Vertiv™ Liebert® GXT5 Lithium-Ion double-conversion online UPS provides the highest level of power conditioning and power protection for critical business IT systems such as network equipment and business servers.",
+      },
+      {
+        type: "structuredCabling",
+        name: "SmartAisle",
+        src: "../../../../assets/images/products/cabling/vertiv/SmartAisle.webp",
+        description: "Liebert SmartAisle is a combination of the most advanced innovations in data center with the simplest self-assembled integrated design that is tailor-made to fit and perform in your white-spaces surpassing your expectations.",
+      },
+  ];
+  
