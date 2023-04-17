099231d698c95e880551575ce7194d4ec359ad46
Khim1111 <124416784+Khim1111@users.noreply.github.com>
2023-04-17 23:20:35 +0800

    Added RedHat products in software solutions page

---
+++ assets/js/product-brands/redhat.js
@@ -0,0 +1,27 @@
+export let brandProducts = [
+    {
+      type: "softwareSolution",
+      name: "Insights",
+      src: "../../../../assets/images/products/software-solution/RedHat/insights.webp",
+      description: "Red Hat® Insights continuously analyzes platforms and applications to predict risk, recommend actions, and track costs so enterprises can better manage hybrid cloud environments.",
+    },
+    {
+      type: "softwareSolution",
+      name: "Certificate System",
+      src: "../../../../assets/images/products/software-solution/RedHat/certificate-system.webp",
+      description: "Red Hat® Certificate System protects internet traffic against hackers and bots by simplifying how a business deploys and adopts public key cryptography—which is responsible for data encryption, decryption, authentication, and more.",
+    },
+    {
+        type: "softwareSolution",
+        name: "Directory Server",
+        src: "../../../../assets/images/products/software-solution/RedHat/directory-server.webp",
+        description: "Easily manage access across partner, supplier, and customer relationships. Storing the user information in a Lightweight Directory Access Protocol (LDAP)-based directory—like Red Hat® Directory Server—makes the system scalable, manageable, and secure.",
+      },
+      {
+        type: "softwareSolution",
+        name: "Enterprise Linux 9",
+        src: "../../../../assets/images/products/software-solution/RedHat/RHEL9.webp",
+        description: "Red Hat Enterprise Linux is the world’s leading enterprise Linux platform,* certified on hundreds of clouds and with thousands of hardware and software vendors. ",
+      },
+    ];
+  
