d9ede67b7bd24ec7b39e5d5fb32586c6ae93d420
Khim1111 <124416784+Khim1111@users.noreply.github.com>
2023-04-19 16:57:04 +0800

    Added vmware products page

---
+++ assets/js/product-brands/vmware.js
@@ -0,0 +1,27 @@
+export let brandProducts = [
+    {
+      type: "softwareSolution",
+      name: "VMware Horizon",
+      src: "../../../../assets/images/products/software-solution/vmware/vm-horizon.webp",
+      description: "Deploy and manage desktops and apps on-premises, or in the public or hybrid cloud with automation and SaaS services from the Horizon Control Plane",
+    },
+    {
+      type: "softwareSolution",
+      name: "NSX Firewall",
+      src: "../../../../assets/images/products/software-solution/vmware/vm-nsx.webp",
+      description: "Secure your multi-cloud environments with a software-based Layer 7 firewall with advanced threat prevention distributed at each workload.",
+    },
+    {
+        type: "softwareSolution",
+        name: "Workstation Pro 17",
+        src: "../../../../assets/images/products/software-solution/vmware/vm-wp17.webp",
+        description: "Run Windows, Linux and BSD virtual machines on a Windows or Linux desktop with VMware Workstation Pro, the industry standard desktop hypervisor.",
+      },
+      {
+        type: "softwareSolution",
+        name: "vSphere",
+        src: "../../../../assets/images/products/software-solution/vmware/vm-sphere.webp",
+        description: "Get the benefits of the cloud for on-premises workloads. vSphere supercharges performance, enhances operational efficiency and accelerates innovation. ",
+      },
+    ];
+  
