3699c74966b189d21d518770dc9516f3cf92d659
Khim1111 <124416784+Khim1111@users.noreply.github.com>
2023-04-19 16:07:04 +0800

    Added Solarwinds products page

--- assets/js/product-brands/redhat.js
+++ assets/js/product-brands/redhat.js
@@ -1,27 +1,27 @@
 export let brandProducts = [
     {
       type: "softwareSolution",
-      name: "Insights",
-      src: "../../../../assets/images/products/software-solution/RedHat/insights.webp",
-      description: "Red Hat® Insights continuously analyzes platforms and applications to predict risk, recommend actions, and track costs so enterprises can better manage hybrid cloud environments.",
+      name: "RHEV-H",
+      src: "../../../../assets/images/products/software-solution/RedHat/rh-rhev.webp",
+      description: "Red Hat® Enterprise Virtualization provides a virtualization software product that includes a hypervisor based on Linux Kernel-based Virtual Machine (KVM), along with a complete virtualization management platform.",
     },
     {
       type: "softwareSolution",
-      name: "Certificate System",
-      src: "../../../../assets/images/products/software-solution/RedHat/certificate-system.webp",
-      description: "Red Hat® Certificate System protects internet traffic against hackers and bots by simplifying how a business deploys and adopts public key cryptography—which is responsible for data encryption, decryption, authentication, and more.",
+      name: "Insights",
+      src: "../../../../assets/images/products/software-solution/RedHat/rh-insight.webp",
+      description: "Red Hat® Insights continuously analyzes platforms and applications to predict risk, recommend actions, and track costs so enterprises can better manage hybrid cloud environments.",
     },
     {
         type: "softwareSolution",
-        name: "Directory Server",
-        src: "../../../../assets/images/products/software-solution/RedHat/directory-server.webp",
-        description: "Easily manage access across partner, supplier, and customer relationships. Storing the user information in a Lightweight Directory Access Protocol (LDAP)-based directory—like Red Hat® Directory Server—makes the system scalable, manageable, and secure.",
+        name: "JBoss",
+        src: "../../../../assets/images/products/software-solution/RedHat/rh-JB.webp",
+        description: "Red Hat® JBoss EAP includes everything needed to build, run, deploy, and manage enterprise Java applications in a variety of environments, including on-premise, virtual environments, and in private, public, and hybrid clouds. JBoss EAP is based upon the popular open source project WildFly.",
       },
       {
         type: "softwareSolution",
         name: "Enterprise Linux 9",
-        src: "../../../../assets/images/products/software-solution/RedHat/RHEL9.webp",
-        description: "Red Hat Enterprise Linux is the world’s leading enterprise Linux platform,* certified on hundreds of clouds and with thousands of hardware and software vendors. ",
+        src: "../../../../assets/images/products/software-solution/RedHat/rh-rhel9.webp",
+        description: "Red Hat® Enterprise Linux is the world’s leading enterprise Linux platform,* certified on hundreds of clouds and with thousands of hardware and software vendors. ",
       },
     ];
   
