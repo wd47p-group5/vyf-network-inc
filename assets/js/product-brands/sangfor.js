a043ddc1d2b36b0c979e9e25717c61f337ea8f5b
Khim1111 <124416784+Khim1111@users.noreply.github.com>
2023-04-20 01:59:05 +0800

    Added Sangfor products page

---
+++ assets/js/product-brands/sangfor.js
@@ -0,0 +1,27 @@
+export let brandProducts = [
+    {
+      type: "dataCenter",
+      name: "aDesk VDI",
+      src: "../../../../assets/images/products/data-center/sangfor/sangfor-adesk_vdi.webp",
+      description: "Through deeply integrated server virtualization, desktop virtualization and storage virtualization, it only takes an aDesk thin client appliance and a cloud terminal to deliver rapid deployment of cloud platform.",
+    },
+    {
+      type: "dataCenter",
+      name: "HCI aServer 2100",
+      src: "../../../../assets/images/products/data-center/sangfor/sangfor-HCI_aServer_2000.webp",
+      description: "Consolidates traditional hardware-appliance-based security, IP network, storage network, server and storage into one tier of commodity hardware (x86 server).",
+    },
+    {
+        type: "dataCenter",
+        name: "S5000-Q-I 1000Mbps WAN ",
+        src: "../../../../assets/images/products/data-center/sangfor/sangfor-S5000-Q-I_1000Mbps_WAN.webp",
+        description: "deployed in the WAN gateway of data centres and branch offices, enables Application Acceleration, reduces traffic, compresses stream, streams cache and accelerates protocol among other leading technologies. ",
+      },
+      {
+        type: "dataCenter",
+        name: "aStor",
+        src: "../../../../assets/images/products/data-center/sangfor/sangfor-astor.webp",
+        description: "uses software-defined technology to consolidate storage into storage resource pool for elastic expansion and on-demand allocation.",
+      },
+    ];
+  
