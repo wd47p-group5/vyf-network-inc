032ddb108f43a065007ed9c960f52a35573e493f
Ed Benedict Quia <edbenedictquia@gmail.com>
2023-04-23 20:51:16 +0800

    test api

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,43 +15,43 @@ let calcScrollValue = () => {
     document.documentElement.scrollTop = 0;
   });
   scrollProgress.style.background = `conic-gradient(#0F9D58 ${scrollValue}%,  #F4B400 ${scrollValue}%)`;
-};    
-  
+};
+
   window.onscroll = calcScrollValue;
   window.onload = calcScrollValue;
 
 // FORM TO GOOGLE SHEETS - Email Subscription
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzfaMzt2aq9PSBfDJ38kEPx3Fr2ePkum8-nr0Ed6waKXmO81B2XCxhVL1Qa1Z2xcQe0/exec'
-const form = document.forms['vyf-subsheet']
-const subscribed = document.getElementById("subscribed")
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbzfaMzt2aq9PSBfDJ38kEPx3Fr2ePkum8-nr0Ed6waKXmO81B2XCxhVL1Qa1Z2xcQe0/exec'
+// const form = document.forms['vyf-subsheet']
+// const subscribed = document.getElementById("subscribed")
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-      subscribed.innerHTML = "<br>Thank you for subscribing!"
-      setTimeout(function(){
-        subscribed.innerHTML = "<br>"
-      },5000)
-      form.reset()
-    })
-    .catch(error => console.error('Error!', error.message))
-})
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
+//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => {
+//       subscribed.innerHTML = "<br>Thank you for subscribing!"
+//       setTimeout(function(){
+//         subscribed.innerHTML = "<br>"
+//       },5000)
+//       form.reset()
+//     })
+//     .catch(error => console.error('Error!', error.message))
+// })
 
-const scriptURL2 = 'https://script.google.com/macros/s/AKfycbwSmDMsrEBQ8DUW2BBF4JoxqmzOMHyeYnyqBu1ZrNfKz_I4gZiMsDRkGqkbuqFMk-hz/exec'
-const form2 = document.forms['vyf-contact-form']
-const message = document.getElementById("message")
+// const scriptURL2 = 'https://script.google.com/macros/s/AKfycbwSmDMsrEBQ8DUW2BBF4JoxqmzOMHyeYnyqBu1ZrNfKz_I4gZiMsDRkGqkbuqFMk-hz/exec'
+// const form2 = document.forms['vyf-contact-form']
+// const message = document.getElementById("message")
 
-form2.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL2, { method: 'POST', body: new FormData(form2)})
-    .then(response => {
-      message.innerHTML = "<br>Thank you for contacting us!"
-      setTimeout(function(){
-        message.innerHTML = "<br>"
-      },5000)
-      form2.reset()
-    })
-    .catch(error => console.error('Error!', error.message))
-})
+// form2.addEventListener('submit', e => {
+//   e.preventDefault()
+//   fetch(scriptURL2, { method: 'POST', body: new FormData(form2)})
+//     .then(response => {
+//       message.innerHTML = "<br>Thank you for contacting us!"
+//       setTimeout(function(){
+//         message.innerHTML = "<br>"
+//       },5000)
+//       form2.reset()
+//     })
+//     .catch(error => console.error('Error!', error.message))
+// })
